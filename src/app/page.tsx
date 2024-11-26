94ecef6ddc11fae0c1388971c2d3bb5e7229691f
nicky <nicolasyanase18@gmail.com>
2024-11-26 03:56:33 -0300

    dashboard finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import {Button} from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { DollarSign, Percent, Users } from "lucide-react";
 import ChartOverview from "@/components/chart";
+import Sales from "@/components/sales";
 export default function Home() {
   return (
     <main className="sm:ml-14 p-4">
@@ -86,9 +87,8 @@ export default function Home() {
       </section>
 
       <section className="mt-4 flex flex-col md:flex-row gap-4">
-        <ChartOverview>
-          
-        </ChartOverview>
+        <ChartOverview/>
+        <Sales/>
       </section>
     </main>
   );
---
+++ src/components/sales/index.tsx
@@ -0,0 +1,65 @@
+import { CircleDollarSign, DollarSign } from "lucide-react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { AvatarImage, Avatar, AvatarFallback } from "../ui/avatar";
+
+export default function Sales(){
+  return(
+    <Card className="flex-1">
+      <CardHeader>
+        <div className="flex items-center justify-center">
+          <CardTitle className="text-lg sm:text-xl text-zinc-800">
+            Ultimos clientes
+          </CardTitle>
+          <CircleDollarSign className="ml-auto size-5"/>
+        </div>
+        <CardDescription>
+        Novos clientes em 24 horas
+      </CardDescription>
+      </CardHeader> 
+        <CardContent>
+          <article className="flex items-center gap-2 border-b py-2">
+            <Avatar className="size-8">
+              <AvatarImage src="https://github.com/Nick-Yanase.png"/>
+              <AvatarFallback>NY</AvatarFallback>
+            </Avatar>
+          <div>
+            <p className="text-sm sm:textbase font-semibold">
+              Nicolas Yanase
+            </p>
+            <span className="text-[12px] sm:text-sm text-zinc-400">
+              nicolasyanase18@gmail.com
+            </span>
+          </div>
+          </article>
+          <article className="flex items-center gap-2 border-b py-2">
+            <Avatar className="size-8">
+              <AvatarImage src="https://github.com/Nick-Yanase.png"/>
+              <AvatarFallback>NY</AvatarFallback>
+            </Avatar>
+          <div>
+            <p className="text-sm sm:textbase font-semibold">
+              Nicolas Yanase
+            </p>
+            <span className="text-[12px] sm:text-sm text-zinc-400">
+              nicolasyanase18@gmail.com
+            </span>
+          </div>
+          </article>
+          <article className="flex items-center gap-2 border-b py-2">
+            <Avatar className="size-8">
+              <AvatarImage src="https://github.com/Nick-Yanase.png"/>
+              <AvatarFallback>NY</AvatarFallback>
+            </Avatar>
+          <div>
+            <p className="text-sm sm:textbase font-semibold">
+              Nicolas Yanase
+            </p>
+            <span className="text-[12px] sm:text-sm text-zinc-400">
+              nicolasyanase18@gmail.com
+            </span>
+          </div>
+          </article>
+        </CardContent>
+    </Card>
+  )
+}
